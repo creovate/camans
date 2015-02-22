fb3cd205b93b86c0d3061d50b85f7fc7b4693e63
nyeinsuaye <sanyein.2012@sis.smu.edu.sg>
2015-02-22 22:06:57 +0800

    report export function

--- src/java/camans/controller/processGenerateReport.java
+++ src/java/camans/controller/processGenerateReport.java
@@ -5,27 +5,40 @@
 package camans.controller;
 
 import camans.dao.ConnectionManager;
+import camans.dao.ReportDAO;
+import camans.entity.JasperDataSourceBuilder;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporterParameter;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.JasperRunManager;
+import net.sf.jasperreports.engine.data.JRBeanArrayDataSource;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.data.JRMapArrayDataSource;
 import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.export.JRCsvExporter;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
 import net.sf.jasperreports.engine.xml.JRXmlLoader;
 import net.sf.jasperreports.view.JasperViewer;
 
@@ -50,50 +63,94 @@ public class processGenerateReport extends HttpServlet {
         try {
             /* TODO output your page here. You may use following sample code. */
             String reportType = request.getParameter("reportType");
-            String reportYearStr = request.getParameter("year");
+            String reportStartStr = request.getParameter("start");
+            String reportEndStr = request.getParameter("end");
 
             ServletOutputStream servletOutputStream = response.getOutputStream();
             HashMap map = new HashMap();
-            
+
+            java.util.Date start = null;
+            java.util.Date end = null;
+
             Connection conn = ConnectionManager.getConnection();
-            if (reportYearStr != null && reportYearStr.length() > 0) {
-                int reportYear = Integer.parseInt(reportYearStr);
-                map.put("year", reportYear);
+            if (reportStartStr != null && reportStartStr.length() > 0 && reportEndStr != null && reportEndStr.length() > 0) {
+                try {
+
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy");
+                    SimpleDateFormat sdfNew = new SimpleDateFormat("MM/dd/yyyy");
+
+                    start = sdf.parse(reportStartStr);
+                    end = sdf.parse(reportEndStr);
+
+                    String startNF = sdfNew.format(start);
+                    String endNF = sdfNew.format(end);
+
+                    start = sdfNew.parse(startNF);
+                    end = sdfNew.parse(endNF);
+
+                    map.put("start", start);
+                    map.put("end", end);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
 
 
             String filePath = null;
-            byte[] bytes = null;
-            
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
             if (reportType.equals("Case Summary")) {
                 //get the file path
                 filePath = getServletContext().getRealPath("/reports/CaseSummaryReport.jasper");
 
                 //generate report
-                bytes = JasperRunManager.runReportToPdf(filePath, map,conn);
+
+                JasperPrint print = JasperFillManager.fillReport(filePath, map);
+
+//                JasperViewer.viewReport(print);
+
+//                JRCsvExporter exporter = new JRCsvExporter();
+//                exporter.setExporterInput(null);
+//                exporter.setParameter(JRExporterParameter.JASPER_PRINT, print);
+//                
+//                exporter.setParameter(JRExporterParameter.OUTPUT_STRING_BUFFER, buffer);
+//                exporter.exportReport();
+
+                JRXlsxExporter exporter = new JRXlsxExporter();
+                exporter.setParameter(JRExporterParameter.JASPER_PRINT, print);
+
+                exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, os);
+                exporter.exportReport();
+
+                response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+                response.setHeader("Content-Disposition", "attachment;filename=CaseSummaryReport.xlsx");
 
 
-            } else {
+            } else if (reportType.equals("Benefit Summary")) {
                 //get the file path
                 filePath = getServletContext().getRealPath("/reports/BenefitSummaryReport.jasper");
-                
+
                 //generate report
-                bytes = JasperRunManager.runReportToPdf(filePath, map, conn);
-                
-            }
-         
-            response.setContentType("application/pdf");
-            response.setContentLength(bytes.length);
-            servletOutputStream.write(bytes, 0, bytes.length);
-            servletOutputStream.flush();
-            servletOutputStream.close();
-           
+                JasperPrint print = JasperFillManager.fillReport(filePath, map);
+
+                JRXlsxExporter exporter = new JRXlsxExporter();
+                exporter.setParameter(JRExporterParameter.JASPER_PRINT, print);
+
+                exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, os);
+                exporter.exportReport();
+
+                response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+                response.setHeader("Content-Disposition", "attachment;filename=BenefitSummaryReport.xlsx");
+
+            } 
+
+            response.getOutputStream().write(os.toByteArray());
+            response.flushBuffer();
         } catch (JRException e) {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
-           //out.close();
+            //out.close();
         }
     }
 
--- web/report.jsp
+++ web/report.jsp
@@ -6,18 +6,31 @@
 <%@ include file="protect.jsp"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
+<%
+User userLogin = (User) session.getAttribute("userLogin");
+
+String userRole = userLogin.getRole();
+boolean isSpecialist = false;
+if(userRole.equals("Specialist") || userRole.equals("Associate")){
+    isSpecialist = true;
+}
+%>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-        <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" />
+        <link rel="stylesheet" href="css/bootstrap.css" media="screen" />
         <link rel="stylesheet" href="css/bootstrap-theme.min.css" media="screen" />
+        <link rel="stylesheet" href="css/jquery-ui-1.9.2.custom.css">
+        <link rel="stylesheet" href="css/jquery-ui.structure.css">
+        <link rel="stylesheet" href="css/jquery-ui.theme.css">
+        <link rel="stylesheet" href="css/jasny-bootstrap.css"/>
         <link rel="stylesheet" href="css/custom.css" media="screen" /> 
 
         <script src="js/jquery-2.1.1.js"></script>
         <script src="js/bootstrap.min.js"></script>
-        <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
-        <script src="js/jquery.steps.js"></script>
+        <script src="js/jquery-ui-1.9.2.custom.js"></script>
+        <script src="js/jquery.steps.js"></script>     
         <!--bootstrap session timeout, added by soemyatmyat-->
         <script src="js/bootstrap-session-timeout.min.js"></script> 
 
@@ -30,13 +43,29 @@
             }
         </style>
         <script>
-            //year dropdown
-            $(document).ready(function() {
-                for (i = new Date().getFullYear(); i > 2000; i--)
-                {
-                    $('#year').append($('<option />').val(i).html(i));
+            $(document).ready(function(){
+                //----validation for start date and end date----//
+            $(".startDate").datepicker({
+                dateFormat: 'dd-M-yy',
+                changeMonth: true,
+                changeYear: true,
+                onClose: function(selectedDate) {
+                    $(".endDate").datepicker("option", "minDate", selectedDate);
                 }
             });
+            $(".endDate").datepicker({
+                dateFormat: 'dd-M-yy',
+                changeMonth: true,
+                changeYear: true,
+                onClose: function(selectedDate) {
+
+                    $(".startDate").datepicker("option", "maxDate", selectedDate);
+                }
+            });
+
+            });
+            
+
         </script>
     </head>
     <body id="report">
@@ -50,35 +79,43 @@
             <br/>
             <h5 style="padding-left: 2%; color: gray">Please choose a report type and year to view reports.</h5><br/>
             <form method="POST" action="generateReport.do" id="report_generate_form" class="form col-sm-12">
-                <label class="col-md-1" for="reportType">Report Type: </label>
-                <div class="col-md-3">
-                    <select class="form-control" id="reportType" name="reportType" required> 
-                        <option value="" disabled selected>Choose a report type..</option>
-                        <option value ="Case Summary">Case Summary</option>
-                        <option value ="Benefit Summary">Benefit Summary</option>
-                    </select>
-                </div>
-                <label class="col-md-1 text-right" for="year">Year: </label>
-                <div class="col-md-3">
-                    <select class="form-control" id="year" name="year" required> 
-                        <option value="" disabled selected>Year..</option>
-                    </select>
+                <div class="form-group col-md-12">
+                    <label class="col-md-1" for="reportType">Report Type: </label>
+                    <div class="col-md-3">
+                        <select class="form-control" id="reportType" name="reportType" required> 
+                            <option value="" disabled selected>Choose a report type..</option>
+                            <option value ="Case Summary">Case Summary</option>
+                            <option value ="Benefit Summary">Benefit Summary</option>
+                            <%
+                            if(!isSpecialist){
+                                %>
+                            <option value ="Clients by nationality and gender">Clients by nationality and gender</option>
+<option value ="Clients by nationality and work pass">Clients by nationality and work pass</option>
+<option value ="Clients by problem and work pass">Clients by problem and work pass</option>
+<option value ="Clients by problem and nationality">Clients by problem and nationality</option>
+                            <%
+                            }
+                            %>
+                        </select>
+                    </div>
+                </div><br/><br/>
+                
+                <div class="form-group col-md-12">
+                    <label class="col-md-1 text-right" for="start">From: </label>
+                    <div class="col-md-3">
+                        <input class="form-control startDate" type='text' name="start" required>
+                    </div>
+                    <label class="col-md-1 text-right" for="end">To: </label>
+                    <div class="col-md-3">
+                        <input type="text" class="form-control endDate" name="end" required/>
+                    </div>
                 </div>
                 <button id="generateSubmitbtn" type="submit" class="btn btn-blue">Generate Report</button>
+
             </form>
         </div>
         <script>
-            //session time out
-            $(document).ready(function() {
-                $.sessionTimeout({
-                    message: 'Your session will be expired in one minute.',
-                    keepAliveUrl: 'keep-alive.html',
-                    logoutUrl: 'index.jsp',
-                    redirUrl: 'logout.jsp',
-                    warnAfter: 900000,
-                    redirAfter: 120000
-                });
-            });
+            
         </script> 
     </body>
 </html>
