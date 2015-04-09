50146ae586a6a7c225bf1ad9a1aa5c20ebf1cc09
xiaomengshi <xiaomengshi.2012@sis.smu.edu.sg>
2015-04-09 18:12:38 +0800

    Update WorkerDigitalContact.java

--- src/java/camans/entity/WorkerDigitalContact.java
+++ src/java/camans/entity/WorkerDigitalContact.java
@@ -22,6 +22,17 @@ public class WorkerDigitalContact {
     private String remark;
     private Date obseleteDate;
 
+    /**
+     *
+     * @param workerFinNum FIN
+     * @param id id
+     * @param contactType contact type
+     * @param contactTypeMore more contact type
+     * @param contactInfo contact information
+     * @param owner owner
+     * @param remark remark
+     * @param obseleteDate obsolete date
+     */
     public WorkerDigitalContact(String workerFinNum, int id, String contactType, String contactTypeMore, String contactInfo, String owner, String remark, Date obseleteDate) {
         this.workerFinNum = workerFinNum;
         this.id = id;
@@ -33,6 +44,16 @@ public class WorkerDigitalContact {
         this.obseleteDate = obseleteDate;
     }
 
+    /**
+     *
+     * @param workerFinNum FIN
+     * @param contactType contact type
+     * @param contactTypeMore more contact type
+     * @param contactInfo contact information
+     * @param owner owner
+     * @param remark remark
+     * @param obseleteDate obsolete date
+     */
     public WorkerDigitalContact(String workerFinNum, String contactType, String contactTypeMore, String contactInfo, String owner, String remark, Date obseleteDate) {
         this.workerFinNum = workerFinNum;
         this.contactType = contactType;
@@ -43,67 +64,130 @@ public class WorkerDigitalContact {
         this.obseleteDate = obseleteDate;
     }
 
+    /**
+     * return FIN
+     * @return FIN
+     */
     public String getWorkerFinNum() {
         return workerFinNum;
     }
 
+    /**
+     * set FIN
+     * @param workerFinNum FIN
+     */
     public void setWorkerFinNum(String workerFinNum) {
         this.workerFinNum = workerFinNum;
     }
 
-    
+    /**
+     * return id
+     * @return id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * set id
+     * @param id id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * return contact type
+     * @return contact type
+     */
     public String getContactType() {
         return contactType;
     }
 
+    /**
+     * set contact type
+     * @param contactType contact type
+     */
     public void setContactType(String contactType) {
         this.contactType = contactType;
     }
 
+    /**
+     * return more contact type
+     * @return more contact type
+     */
     public String getContactTypeMore() {
         return contactTypeMore;
     }
 
+    /**
+     * set more contact type
+     * @param contactTypeMore more contact type
+     */
     public void setContactTypeMore(String contactTypeMore) {
         this.contactTypeMore = contactTypeMore;
     }
 
+    /**
+     * return contact information
+     * @return contact information
+     */
     public String getContactInfo() {
         return contactInfo;
     }
 
+    /**
+     * set contact information
+     * @param contactInfo contact information
+     */
     public void setContactInfo(String contactInfo) {
         this.contactInfo = contactInfo;
     }
 
+    /**
+     * return owner
+     * @return owner
+     */
     public String getOwner() {
         return owner;
     }
 
+    /**
+     * set owner
+     * @param owner owner
+     */
     public void setOwner(String owner) {
         this.owner = owner;
     }
 
+    /**
+     * return remark
+     * @return remark
+     */
     public String getRemark() {
         return remark;
     }
 
+    /**
+     * set remark
+     * @param remark remark
+     */
     public void setRemark(String remark) {
         this.remark = remark;
     }
 
+    /**
+     * return obsolete date
+     * @return obsolete date
+     */
     public Date getObseleteDate() {
         return obseleteDate;
     }
 
+    /**
+     * set obsolete date
+     * @param obseleteDate obsolete date
+     */
     public void setObseleteDate(Date obseleteDate) {
         this.obseleteDate = obseleteDate;
     }
@@ -112,6 +196,7 @@ public class WorkerDigitalContact {
     public String toString() {
         return "WorkerDigitalContact{" + "workerFinNum=" + workerFinNum + ", id=" + id + ", contactType=" + contactType + ", contactTypeMore=" + contactTypeMore + ", contactInfo=" + contactInfo + ", owner=" + owner + ", remark=" + remark + ", obseleteDate=" + obseleteDate + '}';
     }
+    
     
     public String toString2() {
         if (obseleteDate == null) {
