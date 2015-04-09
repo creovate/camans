fe2216c731d20c69752e81b932aec38201341bbb
xiaomengshi <xiaomengshi.2012@sis.smu.edu.sg>
2015-04-09 17:25:33 +0800

    Update ProblemOtherProblems.java

--- src/java/camans/entity/ProblemOtherProblems.java
+++ src/java/camans/entity/ProblemOtherProblems.java
@@ -19,6 +19,16 @@ public class ProblemOtherProblems {
     private double otherProblemLoss;
     private String otherProblemRemark;
 
+    /**
+     *
+     * @param id id
+     * @param workerFinNumber FIN
+     * @param jobKey job key
+     * @param problemKey problem key
+     * @param otherProblemDesc other problem description
+     * @param otherProblemLoss other problem loss
+     * @param otherProblemRemark remark of other problem
+     */
     public ProblemOtherProblems(int id, String workerFinNumber, int jobKey, int problemKey, 
             String otherProblemDesc, double otherProblemLoss, String otherProblemRemark) {
         this.id = id;
@@ -30,6 +40,15 @@ public class ProblemOtherProblems {
         this.otherProblemRemark = otherProblemRemark;
     }
 
+    /**
+     *
+     * @param workerFinNumber FIN
+     * @param jobKey job key
+     * @param problemKey problem key
+     * @param otherProblemDesc other problem description
+     * @param otherProblemLoss other problem loss
+     * @param otherProblemRemark remark of other problem
+     */
     public ProblemOtherProblems(String workerFinNumber, int jobKey, int problemKey, 
             String otherProblemDesc, double otherProblemLoss, String otherProblemRemark) {
         this.workerFinNumber = workerFinNumber;
@@ -40,58 +59,114 @@ public class ProblemOtherProblems {
         this.otherProblemRemark = otherProblemRemark;
     }
 
+    /**
+     * returns id
+     * @return id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * get id
+     * @param id id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * returns FIN
+     * @return FIN
+     */
     public String getWorkerFinNumber() {
         return workerFinNumber;
     }
 
+    /**
+     * set FIN
+     * @param workerFinNumber FIN
+     */
     public void setWorkerFinNumber(String workerFinNumber) {
         this.workerFinNumber = workerFinNumber;
     }
 
+    /**
+     * returns job key
+     * @return job key
+     */
     public int getJobKey() {
         return jobKey;
     }
 
+    /**
+     * set job key
+     * @param jobKey job key
+     */
     public void setJobKey(int jobKey) {
         this.jobKey = jobKey;
     }
 
+    /**
+     * returns problem key
+     * @return problem key
+     */
     public int getProblemKey() {
         return problemKey;
     }
 
+    /**
+     * set problem key
+     * @param problemKey problem key
+     */
     public void setProblemKey(int problemKey) {
         this.problemKey = problemKey;
     }
 
+    /**
+     * returns other problem description
+     * @return other problem description
+     */
     public String getOtherProblemDesc() {
         return otherProblemDesc;
     }
 
+    /**
+     * set other problem description
+     * @param otherProblemDesc other problem description
+     */
     public void setOtherProblemDesc(String otherProblemDesc) {
         this.otherProblemDesc = otherProblemDesc;
     }
 
+    /**
+     * returns loss of other problem
+     * @return loss of other problem
+     */
     public double getOtherProblemLoss() {
         return otherProblemLoss;
     }
 
+    /**
+     * set loss of other problem
+     * @param otherProblemLoss loss of other problem
+     */
     public void setOtherProblemLoss(double otherProblemLoss) {
         this.otherProblemLoss = otherProblemLoss;
     }
 
+    /**
+     * returns remark of other problem
+     * @return remark of other problem
+     */
     public String getOtherProblemRemark() {
         return otherProblemRemark;
     }
 
+    /**
+     * set remark of other problem
+     * @param otherProblemRemark remark of other problem
+     */
     public void setOtherProblemRemark(String otherProblemRemark) {
         this.otherProblemRemark = otherProblemRemark;
     }
@@ -104,6 +179,10 @@ public class ProblemOtherProblems {
                 ", otherProblemRemark=" + otherProblemRemark + '}';
     }
     
+    /**
+     *
+     * @return
+     */
     public String toString2() {
         return "Other Problem [otherProblemDesc=" + otherProblemDesc + ", otherProblemLoss=" + otherProblemLoss + 
                 ", otherProblemRemark=" + otherProblemRemark + ']';
