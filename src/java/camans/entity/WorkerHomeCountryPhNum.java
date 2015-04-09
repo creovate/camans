6a0569c85d0a02b637ee7a7f2ec10310c67899cc
xiaomengshi <xiaomengshi.2012@sis.smu.edu.sg>
2015-04-09 18:20:39 +0800

    Update WorkerHomeCountryPhNum.java

--- src/java/camans/entity/WorkerHomeCountryPhNum.java
+++ src/java/camans/entity/WorkerHomeCountryPhNum.java
@@ -19,6 +19,14 @@ public class WorkerHomeCountryPhNum {
     private String phOwner;
     private Date obseleteDate;
 
+    /**
+     *
+     * @param workerFinNum FIN
+     * @param id id
+     * @param phNumber phone number
+     * @param phOwner phone owner
+     * @param obseleteDate obsolete date
+     */
     public WorkerHomeCountryPhNum(String workerFinNum, int id, String phNumber, String phOwner, Date obseleteDate) {
         this.workerFinNum = workerFinNum;
         this.id = id;
@@ -27,6 +35,13 @@ public class WorkerHomeCountryPhNum {
         this.obseleteDate = obseleteDate;
     }
 
+    /**
+     *
+     * @param workerFinNum FIN
+     * @param phNumber phone number
+     * @param phOwner phone owner
+     * @param obseleteDate obsolete date
+     */
     public WorkerHomeCountryPhNum(String workerFinNum, String phNumber, String phOwner, Date obseleteDate) {
         this.workerFinNum = workerFinNum;
         this.phNumber = phNumber;
@@ -34,42 +49,82 @@ public class WorkerHomeCountryPhNum {
         this.obseleteDate = obseleteDate;
     }
 
+    /**
+     * return FIN
+     * @return FIN
+     */
     public String getWorkerFinNum() {
         return workerFinNum;
     }
 
+    /**
+     * set FIN
+     * @param workerFinNum FIN
+     */
     public void setWorkerFinNum(String workerFinNum) {
         this.workerFinNum = workerFinNum;
     }
 
+    /**
+     * return id
+     * @return id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * set id 
+     * @param id id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * return phone number
+     * @return phone number
+     */
     public String getPhNumber() {
         return phNumber;
     }
 
+    /**
+     * set phone number
+     * @param phNumber phone number
+     */
     public void setPhNumber(String phNumber) {
         this.phNumber = phNumber;
     }
 
+    /**
+     * return phone owner
+     * @return phone owner
+     */
     public String getPhOwner() {
         return phOwner;
     }
 
+    /**
+     * set phone owner
+     * @param phOwner phone owner
+     */
     public void setPhOwner(String phOwner) {
         this.phOwner = phOwner;
     }
 
+    /**
+     * return obsolete date
+     * @return obsolete date
+     */
     public Date getObseleteDate() {
         return obseleteDate;
     }
 
+    /**
+     * set obsolete date
+     * @param obseleteDate obsolete date
+     */
     public void setObseleteDate(Date obseleteDate) {
         this.obseleteDate = obseleteDate;
     }
@@ -79,6 +134,7 @@ public class WorkerHomeCountryPhNum {
         return "WorkerHomeCountryPhNum{" + "workerFinNum=" + workerFinNum + ", id=" + id + ", phNumber=" + phNumber + ", phOwner=" + phOwner + ", obseleteDate=" + obseleteDate + '}';
     }
     
+   
     public String toString2() {
         if (obseleteDate == null) {
             return "Home Country PhNum [phNumber=" + phNumber + ", phOwner=" + phOwner + "]";
