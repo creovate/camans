cdd40c643ad964db4809819584d5ac0ec71f594b
xiaomengshi <xiaomengshi.2012@sis.smu.edu.sg>
2015-04-09 18:30:28 +0800

    Update WorkerNickname.java

--- src/java/camans/entity/WorkerNickname.java
+++ src/java/camans/entity/WorkerNickname.java
@@ -15,39 +15,72 @@ public class WorkerNickname {
     private int id;
     private String nickname;
 
+    /**
+     *
+     * @param workerFinNum FIN
+     * @param id id
+     * @param nickname nickname
+     */
     public WorkerNickname(String workerFinNum, int id, String nickname) {
         this.workerFinNum = workerFinNum;
         this.id = id;
         this.nickname = nickname;
     }
     
+    /**
+     *
+     * @param workerFinNum FIN
+     * @param nickname nickname
+     */
     public WorkerNickname(String workerFinNum, String nickname) {
         this.workerFinNum = workerFinNum;
         this.nickname = nickname;
     }
 
+    /**
+     *return FIN
+     * @return FIN
+     */
     public String getWorkerFinNum() {
         return workerFinNum;
     }
 
+    /**
+     *set FIN
+     * @param workerFinNum FIN
+     */
     public void setWorkerFinNum(String workerFinNum) {
         this.workerFinNum = workerFinNum;
     }
 
-
-
+    /**
+     *return nickname
+     * @return nickname
+     */
     public String getNickname() {
         return nickname;
     }
 
+    /**
+     *set nickname
+     * @param nickname nickname
+     */
     public void setNickname(String nickname) {
         this.nickname = nickname;
     }
 
+    /**
+     *return id
+     * @return id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     *set id
+     * @param id id
+     */
     public void setId(int id) {
         this.id = id;
     }
@@ -57,6 +90,7 @@ public class WorkerNickname {
         return "WorkerNickname{" + "workerFinNum=" + workerFinNum + ", id=" + id + ", nickname=" + nickname + '}';
     }
     
+   
     public String toString2() {
         return "Nickname [nickname=" + nickname + ']';
     }    
