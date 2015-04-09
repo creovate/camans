6696d735e24580821c191644140a751cea2d702c
xiaomengshi <xiaomengshi.2012@sis.smu.edu.sg>
2015-04-09 17:48:57 +0800

    Update ProblemWicaClaim.java

--- src/java/camans/entity/ProblemWicaClaim.java
+++ src/java/camans/entity/ProblemWicaClaim.java
@@ -23,7 +23,21 @@ public class ProblemWicaClaim {
     private String wicaClaimRemark;
 
     //constructor with id
-    public ProblemWicaClaim(int id, String workerFinNumber, int jobKey, int problmeKey, Date wicaClaimDate, String wicaReferenceNumber, String wicaInsurer, String wicaPolicyNumber, String wicaClaimReason, String wicaClaimRemark) {
+
+    /**
+     *
+     * @param id id
+     * @param workerFinNumber FIN
+     * @param jobKey job key
+     * @param problmeKey problem key
+     * @param wicaClaimDate wica claim date
+     * @param wicaReferenceNumber wica claim reference number
+     * @param wicaInsurer wica insurer
+     * @param wicaPolicyNumber wica policy number
+     * @param wicaClaimReason reason of wica claim
+     * @param wicaClaimRemark remark of wica claim
+     */
+        public ProblemWicaClaim(int id, String workerFinNumber, int jobKey, int problmeKey, Date wicaClaimDate, String wicaReferenceNumber, String wicaInsurer, String wicaPolicyNumber, String wicaClaimReason, String wicaClaimRemark) {
             this.id = id;
             this.workerFinNumber = workerFinNumber;
             this.jobKey = jobKey;
@@ -37,7 +51,20 @@ public class ProblemWicaClaim {
     }
 
     //constructor without id
-    public ProblemWicaClaim(String workerFinNumber, int jobKey, int problmeKey, Date wicaClaimDate, String wicaReferenceNumber, String wicaInsurer, String wicaPolicyNumber, String wicaClaimReason, String wicaClaimRemark) {
+
+    /**
+     *
+     * @param workerFinNumber FIN
+     * @param jobKey job key
+     * @param problmeKey problem key
+     * @param wicaClaimDate wica claim date
+     * @param wicaReferenceNumber wica claim reference number
+     * @param wicaInsurer wica insurer
+     * @param wicaPolicyNumber wica policy number
+     * @param wicaClaimReason reason of wica claim
+     * @param wicaClaimRemark remark of wica claim
+     */
+        public ProblemWicaClaim(String workerFinNumber, int jobKey, int problmeKey, Date wicaClaimDate, String wicaReferenceNumber, String wicaInsurer, String wicaPolicyNumber, String wicaClaimReason, String wicaClaimRemark) {
             this.workerFinNumber = workerFinNumber;
             this.jobKey = jobKey;
             this.problmeKey = problmeKey;
@@ -49,82 +76,162 @@ public class ProblemWicaClaim {
             this.wicaClaimRemark = wicaClaimRemark;
     }
 
+    /**
+     * return id
+     * @return id
+     */
     public int getId() {
             return id;
     }
 
+    /**
+     * return FIN
+     * @return FIN
+     */
     public String getWorkerFinNumber() {
             return workerFinNumber;
     }
 
+    /**
+     * return job key
+     * @return job key
+     */
     public int getJobKey() {
             return jobKey;
     }
 
+    /**
+     * return problem key
+     * @return problem key
+     */
     public int getProblmeKey() {
             return problmeKey;
     }
 
+    /**
+     * return  wica claim date
+     * @return wica claim date
+     */
     public Date getWicaClaimDate() {
             return wicaClaimDate;
     }
 
+    /**
+     * return  wica reference number
+     * @return wica reference number
+     */
     public String getWicaReferenceNumber() {
             return wicaReferenceNumber;
     }
 
+    /**
+     * return wica insurer
+     * @return wica insurer
+     */
     public String getWicaInsurer() {
             return wicaInsurer;
     }
 
+    /**
+     * return wica policy number
+     * @return wica policy number
+     */
     public String getWicaPolicyNumber() {
             return wicaPolicyNumber;
     }
 
+    /**
+     * return reason of wica claim 
+     * @return reason of wica claim 
+     */
     public String getWicaClaimReason() {
             return wicaClaimReason;
     }
 
+    /**
+     * return remark of wica claim
+     * @return remark of wica claim
+     */
     public String getWicaClaimRemark() {
             return wicaClaimRemark;
     }
 
+    /**
+     * set id
+     * @param id id
+     */
     public void setId(int id) {
             this.id = id;
     }
 
+    /**
+     * set FIN
+     * @param workerFinNumber FIN
+     */
     public void setWorkerFinNumber(String workerFinNumber) {
             this.workerFinNumber = workerFinNumber;
     }
 
+    /**
+     * set job key
+     * @param jobKey job key
+     */
     public void setJobKey(int jobKey) {
             this.jobKey = jobKey;
     }
 
+    /**
+     * set problem key
+     * @param problmeKey problem key
+     */
     public void setProblmeKey(int problmeKey) {
             this.problmeKey = problmeKey;
     }
 
+    /**
+     * set wica claim date
+     * @param wicaClaimDate wica claim date
+     */
     public void setWicaClaimDate(Date wicaClaimDate) {
             this.wicaClaimDate = wicaClaimDate;
     }
 
+    /**
+     * set wica reference number
+     * @param wicaReferenceNumber wica reference number
+     */
     public void setWicaReferenceNumber(String wicaReferenceNumber) {
             this.wicaReferenceNumber = wicaReferenceNumber;
     }
 
+    /**
+     * set wica insurer
+     * @param wicaInsurer wica insurer
+     */
     public void setWicaInsurer(String wicaInsurer) {
             this.wicaInsurer = wicaInsurer;
     }
 
+    /**
+     * set wica policy number
+     * @param wicaPolicyNumber wica policy number
+     */
     public void setWicaPolicyNumber(String wicaPolicyNumber) {
             this.wicaPolicyNumber = wicaPolicyNumber;
     }
 
+    /**
+     * set reason of wica claim 
+     * @param wicaClaimReason reason of wica claim 
+     */
     public void setWicaClaimReason(String wicaClaimReason) {
             this.wicaClaimReason = wicaClaimReason;
     }
 
+    /**
+     * set remark of wica claim
+     * @param wicaClaimRemark remark of wica claim
+     */
     public void setWicaClaimRemark(String wicaClaimRemark) {
             this.wicaClaimRemark = wicaClaimRemark;
     }
@@ -138,6 +245,10 @@ public class ProblemWicaClaim {
                 ", wicaClaimRemark=" + wicaClaimRemark + '}';
     }
 
+    /**
+     *
+     * @return
+     */
     public String toString2() {
         return "Wica Claim[wicaClaimDate=" + wicaClaimDate + 
                 ", wicaReferenceNumber=" + wicaReferenceNumber + ", wicaInsurer=" + wicaInsurer + 
