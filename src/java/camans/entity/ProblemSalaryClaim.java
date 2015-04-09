e6ccb8ec02e378e702750cac687b8b0abafde870
xiaomengshi <xiaomengshi.2012@sis.smu.edu.sg>
2015-04-09 17:33:42 +0800

    Update ProblemSalaryClaim.java

--- src/java/camans/entity/ProblemSalaryClaim.java
+++ src/java/camans/entity/ProblemSalaryClaim.java
@@ -21,7 +21,18 @@ public class ProblemSalaryClaim {
     private String salaryClaimBasis;
 
     //constructor with id
-    public ProblemSalaryClaim(int id, String workerFinNumber, int jobKey, int problemKey, Date salaryClaimDate, double salaryClaimLoss, String salaryClaimBasis) {
+
+    /**
+     *
+     * @param id id
+     * @param workerFinNumber FIN
+     * @param jobKey job key
+     * @param problemKey problem key
+     * @param salaryClaimDate salary claim date
+     * @param salaryClaimLoss monetary loss
+     * @param salaryClaimBasis claim basis
+     */
+        public ProblemSalaryClaim(int id, String workerFinNumber, int jobKey, int problemKey, Date salaryClaimDate, double salaryClaimLoss, String salaryClaimBasis) {
             this.id = id;
             this.workerFinNumber = workerFinNumber;
             this.jobKey = jobKey;
@@ -32,7 +43,17 @@ public class ProblemSalaryClaim {
     }
 
     //constructor without id
-    public ProblemSalaryClaim(String workerFinNumber, int jobKey, int problemKey, Date salaryClaimDate, double salaryClaimLoss, String salaryClaimBasis) {
+
+    /**
+     *
+    * @param workerFinNumber FIN
+     * @param jobKey job key
+     * @param problemKey problem key
+     * @param salaryClaimDate salary claim date
+     * @param salaryClaimLoss monetary loss
+     * @param salaryClaimBasis claim basis
+     */
+        public ProblemSalaryClaim(String workerFinNumber, int jobKey, int problemKey, Date salaryClaimDate, double salaryClaimLoss, String salaryClaimBasis) {
             this.workerFinNumber = workerFinNumber;
             this.jobKey = jobKey;
             this.problemKey = problemKey;
@@ -41,58 +62,114 @@ public class ProblemSalaryClaim {
             this.salaryClaimBasis = salaryClaimBasis;
     }
 
+    /**
+     * returns id
+     * @return id
+     */
     public int getId() {
             return id;
     }
 
+    /**
+     * returns FIN
+     * @return FIN
+     */
     public String getWorkerFinNumber() {
             return workerFinNumber;
     }
 
+    /**
+     * returns job key
+     * @return job key
+     */
     public int getJobKey() {
             return jobKey;
     }
 
+    /**
+     * returns problem key
+     * @return problem key
+     */
     public int getProblemKey() {
             return problemKey;
     }
 
+    /**
+     * returns salary claim date
+     * @return salary claim date
+     */
     public Date getSalaryClaimDate() {
             return salaryClaimDate;
     }
 
+    /**
+     * returns monetary loss
+     * @return monetary loss
+     */
     public double getSalaryClaimLoss() {
             return salaryClaimLoss;
     }
 
+    /**
+     * return salary claim basis
+     * @return salary claim basis
+     */
     public String getSalaryClaimBasis() {
             return salaryClaimBasis;
     }
 
+    /**
+     *
+     * @param id
+     */
     public void setId(int id) {
             this.id = id;
     }
 
+    /**
+     *
+     * @param workerFinNumber
+     */
     public void setWorkerFinNumber(String workerFinNumber) {
             this.workerFinNumber = workerFinNumber;
     }
 
+    /**
+     * set job key
+     * @param jobKey job key
+     */
     public void setJobKey(int jobKey) {
             this.jobKey = jobKey;
     }
 
+    /**
+     * set problem key
+     * @param problemKey problem key
+     */
     public void setProblemKey(int problemKey) {
             this.problemKey = problemKey;
     }
 
+    /**
+     * set salary claim date
+     * @param salaryClaimDate salary claim date
+     */
     public void setSalaryClaimDate(Date salaryClaimDate) {
             this.salaryClaimDate = salaryClaimDate;
     }
 
+    /**
+     * set monetary loss
+     * @param salaryClaimLoss monetary loss
+     */
     public void setSalaryClaimLoss(double salaryClaimLoss) {
             this.salaryClaimLoss = salaryClaimLoss;
     }
 
+    /**
+     * set salary claim basis
+     * @param salaryClaimBasis salary claim basis
+     */
     public void setSalaryClaimBasis(String salaryClaimBasis) {
             this.salaryClaimBasis = salaryClaimBasis;
     }				
@@ -105,6 +182,10 @@ public class ProblemSalaryClaim {
                 ", salaryClaimBasis=" + salaryClaimBasis + '}';
     }
 
+    /**
+     *
+     * @return
+     */
     public String toString2() {
         return "Salary Claim [salaryClaimDate=" + salaryClaimDate + ", salaryClaimLoss=" + salaryClaimLoss + 
                 ", salaryClaimBasis=" + salaryClaimBasis + ']';
