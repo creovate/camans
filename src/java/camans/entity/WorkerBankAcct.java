f26669b8b61e145a7fcac020c595989ba9fa1820
xiaomengshi <xiaomengshi.2012@sis.smu.edu.sg>
2015-04-09 18:09:36 +0800

    Update WorkerBankAcct.java

--- src/java/camans/entity/WorkerBankAcct.java
+++ src/java/camans/entity/WorkerBankAcct.java
@@ -25,6 +25,20 @@ public class WorkerBankAcct {
     private String remark;
     private Date obseleteDate;
 
+    /**
+     *
+     * @param workerFinNum FIN
+     * @param id id
+     * @param bankAcctName bank account name
+     * @param bankAcctNumber bank account number
+     * @param bankName bank name
+     * @param bankBranchName bank branch name
+     * @param bankBranchAddress bank branch address
+     * @param bankBranchCode bank branch code
+     * @param bankSwiftCode bank swith code
+     * @param remark remark
+     * @param obseleteDate obsolete date
+     */
     public WorkerBankAcct(String workerFinNum, int id, String bankAcctName, String bankAcctNumber, String bankName, String bankBranchName, String bankBranchAddress, String bankBranchCode, String bankSwiftCode, String remark, Date obseleteDate) {
         this.workerFinNum = workerFinNum;
         this.id = id;
@@ -39,6 +53,19 @@ public class WorkerBankAcct {
         this.obseleteDate = obseleteDate;
     }
 
+    /**
+     *
+     * @param workerFinNum FIN
+     * @param bankAcctName bank account name
+     * @param bankAcctNumber bank account number
+     * @param bankName bank name
+     * @param bankBranchName bank branch name
+     * @param bankBranchAddress bank branch address
+     * @param bankBranchCode bank branch code
+     * @param bankSwiftCode bank swith code
+     * @param remark remark
+     * @param obseleteDate obsolete date
+     */
     public WorkerBankAcct(String workerFinNum, String bankAcctName, String bankAcctNumber, String bankName, String bankBranchName, String bankBranchAddress, String bankBranchCode, String bankSwiftCode, String remark, Date obseleteDate) {
         this.workerFinNum = workerFinNum;
         this.bankAcctName = bankAcctName;
@@ -52,90 +79,178 @@ public class WorkerBankAcct {
         this.obseleteDate = obseleteDate;
     }
 
+    /**
+     * return FIN
+     * @return FIN
+     */
     public String getWorkerFinNum() {
         return workerFinNum;
     }
 
+    /**
+     * set FIN
+     * @param workerFinNum FIN
+     */
     public void setWorkerFinNum(String workerFinNum) {
         this.workerFinNum = workerFinNum;
     }
 
+    /**
+     * return id
+     * @return id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * set id
+     * @param id id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * return bank account name
+     * @return bank account name
+     */
     public String getBankAcctName() {
         return bankAcctName;
     }
 
+    /**
+     * set bank account name
+     * @param bankAcctName bank account name
+     */
     public void setBankAcctName(String bankAcctName) {
         this.bankAcctName = bankAcctName;
     }
 
+    /**
+     * return bank account number
+     * @return bank account number
+     */ 
     public String getBankAcctNumber() {
         return bankAcctNumber;
     }
 
+    /**
+     * set bank account number
+     * @param bankAcctNumber bank account number
+     */
     public void setBankAcctNumber(String bankAcctNumber) {
         this.bankAcctNumber = bankAcctNumber;
     }
 
+    /**
+     * return bank name
+     * @return bank name
+     */
     public String getBankName() {
         return bankName;
     }
 
+    /**
+     * set bank name
+     * @param bankName bank name
+     */
     public void setBankName(String bankName) {
         this.bankName = bankName;
     }
 
+    /**
+     * return bank branch name
+     * @return bank branch name
+     */ 
     public String getBankBranchName() {
         return bankBranchName;
     }
 
+    /**
+     * set bank branch name
+     * @param bankBranchName bank branch name
+     */
     public void setBankBranchName(String bankBranchName) {
         this.bankBranchName = bankBranchName;
     }
 
+    /**
+     * return bank branch address
+     * @return bank branch address
+     */
     public String getBankBranchAddress() {
         return bankBranchAddress;
     }
 
+    /**
+     * set bank branch address
+     * @param bankBranchAddress bank branch address
+     */
     public void setBankBranchAddress(String bankBranchAddress) {
         this.bankBranchAddress = bankBranchAddress;
     }
 
+    /**
+     * return bank branch code
+     * @return  bank branch code
+     */
     public String getBankBranchCode() {
         return bankBranchCode;
     }
 
+    /**
+     * set bank branch code
+     * @param bankBranchCode bank branch code
+     */
     public void setBankBranchCode(String bankBranchCode) {
         this.bankBranchCode = bankBranchCode;
     }
 
+    /**
+     * return bank swift code
+     * @return bank swift code
+     */
     public String getBankSwiftCode() {
         return bankSwiftCode;
     }
 
+    /**
+     * set bank swift code
+     * @param bankSwiftCode bank swift code
+     */
     public void setBankSwiftCode(String bankSwiftCode) {
         this.bankSwiftCode = bankSwiftCode;
     }
 
+    /**
+     * return remark
+     * @return remark
+     */
     public String getRemark() {
         return remark;
     }
 
+    /**
+     * set remark
+     * @param remark remark
+     */
     public void setRemark(String remark) {
         this.remark = remark;
     }
 
+    /**
+     * return obsolete date
+     * @return obsolete date
+     */
     public Date getObseleteDate() {
         return obseleteDate;
     }
 
+    /**
+     * set obsolete date
+     * @param obseleteDate obsolete date
+     */
     public void setObseleteDate(Date obseleteDate) {
         this.obseleteDate = obseleteDate;
     }
@@ -145,6 +260,7 @@ public class WorkerBankAcct {
         return "WorkerBankAcct{" + "workerFinNum=" + workerFinNum + ", id=" + id + ", bankAcctName=" + bankAcctName + ", bankAcctNumber=" + bankAcctNumber + ", bankName=" + bankName + ", bankBranchName=" + bankBranchName + ", bankBranchAddress=" + bankBranchAddress + ", bankBranchCode=" + bankBranchCode + ", bankSwiftCode=" + bankSwiftCode + ", remark=" + remark + ", obseleteDate=" + obseleteDate + '}';
     }
  
+    
     public String toString2() {
         if (obseleteDate == null) {
             return "WorkerBankAcct [bankAcctName=" + bankAcctName + ", bankAcctNumber=" + bankAcctNumber + ", bankName=" + bankName + ", bankBranchName=" + bankBranchName + ", bankBranchAddress=" + bankBranchAddress + ", bankBranchCode=" + bankBranchCode + ", bankSwiftCode=" + bankSwiftCode + ", remark=" + remark + ']';
