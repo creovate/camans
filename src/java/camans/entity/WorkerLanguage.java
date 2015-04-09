23db8257f3537f33883c3a30c18c3ec3e23e7955
xiaomengshi <xiaomengshi.2012@sis.smu.edu.sg>
2015-04-09 18:26:19 +0800

    Update WorkerLanguage.java

--- src/java/camans/entity/WorkerLanguage.java
+++ src/java/camans/entity/WorkerLanguage.java
@@ -19,6 +19,15 @@ public class WorkerLanguage {
     private String spokenLanguageStandard;
     private String remark;
 
+    /**
+     *
+     * @param workerFinNum FIN
+     * @param id id
+     * @param mainLanguage main language
+     * @param mainLangaugeMore more main language
+     * @param spokenLanguageStandard spoken language standard
+     * @param remark remark
+     */
     public WorkerLanguage(String workerFinNum, int id, String mainLanguage, String mainLangaugeMore, String spokenLanguageStandard, String remark) {
         this.workerFinNum = workerFinNum;
         this.id = id;
@@ -28,6 +37,14 @@ public class WorkerLanguage {
         this.remark = remark;
     }
 
+    /**
+     *
+     * @param workerFinNum FIN
+    * @param mainLanguage main language
+     * @param mainLangaugeMore more main language
+     * @param spokenLanguageStandard spoken language standard
+     * @param remark remark
+     */
     public WorkerLanguage(String workerFinNum, String mainLanguage, String mainLangaugeMore, String spokenLanguageStandard, String remark) {
         this.workerFinNum = workerFinNum;
         this.mainLanguage = mainLanguage;
@@ -36,50 +53,98 @@ public class WorkerLanguage {
         this.remark = remark;
     }
 
+    /**
+     * return FIN
+     * @return  FIN
+     */
     public String getWorkerFinNum() {
         return workerFinNum;
     }
 
+    /**
+     * set FIN
+     * @param workerFinNum FIN
+     */
     public void setWorkerFinNum(String workerFinNum) {
         this.workerFinNum = workerFinNum;
     }
 
+    /**
+     * return id
+     * @return id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * set id
+     * @param id id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * return main language
+     * @return main language
+     */
     public String getMainLanguage() {
         return mainLanguage;
     }
 
+    /**
+     * set main language
+     * @param mainLanguage main language
+     */
     public void setMainLanguage(String mainLanguage) {
         this.mainLanguage = mainLanguage;
     }
 
+    /**
+     * return more main language
+     * @return more main language
+     */
     public String getMainLangaugeMore() {
         return mainLangaugeMore;
     }
 
+    /**
+     * set more main language
+     * @param mainLangaugeMore more main language
+     */
     public void setMainLangaugeMore(String mainLangaugeMore) {
         this.mainLangaugeMore = mainLangaugeMore;
     }
 
+    /**
+     * get spoken language standard
+     * @return spoken language standard
+     */
     public String getSpokenLanguageStandard() {
         return spokenLanguageStandard;
     }
 
+    /**
+     * set spoken language standard
+     * @param spokenLanguageStandard spoken language standard
+     */
     public void setSpokenLanguageStandard(String spokenLanguageStandard) {
         this.spokenLanguageStandard = spokenLanguageStandard;
     }
 
+    /**
+     * return remark
+     * @return remark
+     */
     public String getRemark() {
         return remark;
     }
 
+    /**
+     * set remark
+     * @param remark remark
+     */
     public void setRemark(String remark) {
         this.remark = remark;
     }
@@ -88,6 +153,7 @@ public class WorkerLanguage {
     public String toString() {
         return "WorkerLanguage{" + "workerFinNum=" + workerFinNum + ", id=" + id + ", mainLanguage=" + mainLanguage + ", mainLangaugeMore=" + mainLangaugeMore + ", spokenLanguageStandard=" + spokenLanguageStandard + ", remark=" + remark + '}';
     }
+    
     
     public String toString2() {
         return "WorkerLanguage [mainLanguage=" + mainLanguage + ", mainLangaugeMore=" + mainLangaugeMore + ", spokenLanguageStandard=" + spokenLanguageStandard + ", remark=" + remark + ']';
