86f22e9032056b7aadb6e8c193efe37f01df9b97
xiaomengshi <xiaomengshi.2012@sis.smu.edu.sg>
2015-04-09 18:05:49 +0800

    Update WorkerAttachment.java

--- src/java/camans/entity/WorkerAttachment.java
+++ src/java/camans/entity/WorkerAttachment.java
@@ -17,6 +17,14 @@ public class WorkerAttachment {
     private String filePath;
     private String submitBy;
 
+    /**
+     *
+     * @param workerFinNum FIN
+     * @param id id
+     * @param documentName document name
+     * @param filePath file path
+     * @param submitBy submit by
+     */
     public WorkerAttachment(String workerFinNum, int id, String documentName, String filePath, String submitBy) {
         this.workerFinNum = workerFinNum;
         this.id = id;
@@ -25,6 +33,13 @@ public class WorkerAttachment {
         this.submitBy = submitBy;
     }
 
+    /**
+     *
+     * @param workerFinNum FIN
+     * @param documentName document name
+     * @param filePath file path
+     * @param submitBy submit by
+     */
     public WorkerAttachment(String workerFinNum, String documentName, String filePath, String submitBy) {
         this.workerFinNum = workerFinNum;
         this.documentName = documentName;
@@ -32,42 +47,82 @@ public class WorkerAttachment {
         this.submitBy = submitBy;
     }
 
+    /**
+     * return FIN
+     * @return FIN
+     */
     public String getWorkerFinNum() {
         return workerFinNum;
     }
 
+    /**
+     * set FIN
+     * @param workerFinNum FIN
+     */
     public void setWorkerFinNum(String workerFinNum) {
         this.workerFinNum = workerFinNum;
     }
 
+    /**
+     * return id
+     * @return id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * set id
+     * @param id id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * return document name
+     * @return document name
+     */
     public String getDocumentName() {
         return documentName;
     }
 
+    /**
+     * set document name
+     * @param documentName document name
+     */
     public void setDocumentName(String documentName) {
         this.documentName = documentName;
     }
 
+    /**
+     * return file path
+     * @return file path
+     */
     public String getFilePath() {
         return filePath;
     }
 
+    /**
+     * set file path
+     * @param filePath file path
+     */
     public void setFilePath(String filePath) {
         this.filePath = filePath;
     }
 
+    /**
+     * return submit by
+     * @return submit by
+     */
     public String getSubmitBy() {
         return submitBy;
     }
 
+    /**
+     * set submit by
+     * @param submitBy submit by
+     */
     public void setSubmitBy(String submitBy) {
         this.submitBy = submitBy;
     }
@@ -78,6 +133,7 @@ public class WorkerAttachment {
                 ", documentName=" + documentName + ", filePath=" + filePath + 
                 ", submitBy=" + submitBy + '}';
     }
+    
     
     public String toString2() {
         return "Attachment [documentName=" + documentName + ']';
